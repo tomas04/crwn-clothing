935411812e866c807a4e5662d74edc690a6eedcc
Tomas <tomas87@gmail.com>
2020-01-10 10:35:55 +0100

    Added mock data

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,47 +1,15 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
+import SHOP_DATA from "./mock_data";
 
 class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: [
-        {
-          title: "hats",
-          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-          id: 1,
-          linkUrl: "shop/hats"
-        },
-        {
-          title: "jackets",
-          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-          id: 2,
-          linkUrl: "shop/jackets"
-        },
-        {
-          title: "sneakers",
-          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-          id: 3,
-          linkUrl: "shop/sneakers"
-        },
-        {
-          title: "womens",
-          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-          size: "large",
-          id: 4,
-          linkUrl: "shop/womens"
-        },
-        {
-          title: "mens",
-          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-          size: "large",
-          id: 5,
-          linkUrl: "shop/mens"
-        }
-      ]
-    };
+      sections: SHOP_DATA
+    }
   }
 
   render() {
@@ -53,6 +21,7 @@ class Directory extends Component {
       </div>
     );
   }
+
 }
 
 export default Directory;
