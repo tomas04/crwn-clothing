f98b914613a8b01fca237f3907515e2fc1aac416
Tomas Johansen <tomas87@gmail.com>
2020-01-13 09:59:25 +0100

    Added shop, collection preview and collection item. Also turned on debugging.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
-import SHOP_DATA from "./mock_data";
 
 class Directory extends Component {
   constructor() {
     super();
 
     this.state = {
-      sections: SHOP_DATA
-    }
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "shop/hats"
+        },
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets"
+        },
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers"
+        },
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens"
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens"
+        }
+      ]
+    };
   }
 
   render() {
@@ -21,7 +53,6 @@ class Directory extends Component {
       </div>
     );
   }
-
 }
 
 export default Directory;
